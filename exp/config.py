55c5305cc78d49ecb96501de619a404d3458ca70
RUAN0007 <ruanpingcheng@gmail.com>
2018-06-01 14:22:17 +0800

    Add experiment script

---
+++ exp/config.py
@@ -0,0 +1,45 @@
+THREADS=[16]
+TXRATES=[32]
+BOOTSTRAP_HOSTNO=50     # <-- where there server (running HL) is
+CLIENT_HOSTNO=90        # <-- where the driver/client is
+HOST="slave-{}"
+
+HL_FILESYSTEMPATH="/data/ruanpc/hyperledger"   # <-- where the HL is store (for rocksdb)
+                                          # for UStore, data is store in
+                                          # $HL/fabric/build/bin/ustore_data
+USTORE_DATAPATH="/data/ruanpc/ustore_data"
+
+CLIENT_EXE="/users/ruanpc/blockbench/src/macro/smallbank/driver"   # <-- where this current directory is
+PEER_EXE="/users/ruanpc/gopath/src/github.com/hyperledger/fabric/build/bin/peer"
+
+PEER_LOG_PATH = "/data/ruanpc/hl_log_{}nodes_{}"
+CLI_LOG_PATH = "/data/ruanpc/cli_log_{}nodes_{}rate_{}threads_{}"
+
+LOGGING_LEVEL = "warning:consensus/pbft,consensus/executor,ledger,db,buckettree,state,consensus/statetransfer=info"
+
+ENV_TEMPLATE = ("CORE_PEER_ID=vp{} CORE_PEER_ADDRESSAUTODETECT=true "
+                "CORE_PEER_NETWORK=blockbench "
+                "CORE_PEER_VALIDATOR_CONSENSUS_PLUGIN=pbft "
+                "CORE_PEER_FILE_SYSTEM_PATH="+HL_FILESYSTEMPATH+" "
+                "CORE_VM_ENDPOINT=http://localhost:2375 "
+                "CORE_PBFT_GENERAL_MODE=batch "
+                "CORE_PBFT_GENERAL_TIMEOUT_BATCH=1s "
+                "CORE_PBFT_GENERAL_TIMEOUT_REQUEST=2s "
+                "CORE_PBFT_GENERAL_TIMEOUT_VIEWCHANGE=2s "
+                "CORE_PBFT_GENERAL_TIMEOUT_RESENDVIEWCHANGE=1s "
+                "CORE_PBFT_GENERAL_N={} "
+                "CORE_PBFT_GENERAL_F=0 "
+                "LEDGER_SAMPLE_INTERVAL=127 "
+                "CORE_EXECUTION_SAMPLE_INTERVAL=127 "
+                "CORE_PBFT_GENERAL_BATCHSIZE={} "
+                "CORE_PEER_DB_DBTYPE={} "
+                "CORE_PEER_DB_COMPRESSION=false")
+
+ENV_EXTRA = "CORE_PEER_DISCOVERY_ROOTNODE={}:7051"
+
+
+CMD="\" removeUnwantedImages; rm -rf {};  {} nohup " + PEER_EXE + " node start --logging-level= " + LOGGING_LEVEL + " > {} 2>&1 &\""
+KILL_SERVER_CMD = "pkill -TERM peer"
+
+CLIENT_CMD = "\"" + CLIENT_EXE + " {} {} {} 100 {} github.com/smallbank > {} 2>&1 &\""
+KILL_CLIENT_CMD = "killall -KILL driver"
---
+++ exp/smallbank-exp.py
@@ -0,0 +1,123 @@
+# args: <workload file> <batchsize> <sample rate (ledger)> <sample rate (commit)>
+# sample rate: 0 -> get data every sample
+#             ~(1<<x) -> get data every 2^x samples
+# outputs:
+  #
+import sys
+import os
+import time
+from config import *
+
+usage = '''Usage:
+    smallbank-exp.py <db> <num-node> <num-txn> <num-threads> <txn-rate> <batch size>
+
+      db:                       either original, ustore or rocksdb
+      num-node:                 total number of nodes
+      num-txn:                  total number of txns
+      num-threads:              total number of client threads
+      txn-rate:                 txn rate
+      batch size:               block size
+    '''
+
+def run_servers(nodes, cmds):
+  for i in range(len(nodes)):
+    cmd="ssh {} {}".format(nodes[i], cmds[i])
+    print cmd
+    os.system(cmd)
+
+# def run_clients(client, server, num_node, num_txn, num_thread, txrate):
+#   client_log_path = CLI_LOG_PATH.format(num_node, txrate, num_thread)
+#   endpoint = server +
+#   client_cmd = CLIENT_CMD.format(num_txn, num_thread, txrate, endpoint, client_log_path)
+#   cmd = "ssh {} {}".format(client, client_cmd)
+#   print cmd
+  # os.system(cmd)
+
+def stop(client, servers):
+  # kill_client = "ssh {} {}".format(client, KILL_CLIENT_CMD)
+  # print kill_client
+  # os.system(kill_client)
+  for s in servers:
+    kill_server = "ssh {} {}".format(s, KILL_SERVER_CMD)
+    print kill_server
+    os.system(kill_server)
+
+def run_exp(num_node, num_txn, num_thread, txrate, batch_size, db_type):
+  print "Start to run experiment. \n"
+  N = num_node
+
+  nodes=[]
+  nodes.append(HOST.format(BOOTSTRAP_HOSTNO))
+  for i in range(1,N):
+    nodes.append(HOST.format(BOOTSTRAP_HOSTNO+i))
+
+  cmds =[] # list of commands
+  for i in range(N):
+    env = ENV_TEMPLATE.format(i, N, batch_size, db_type)
+    if i > 0:
+      # add discoverynode
+      env = "{} {}".format(env, ENV_EXTRA.format(nodes[0]))
+
+    peer_log_path = PEER_LOG_PATH.format(num_node, db_type)
+    if db_type == "rocksdb" or db_type == "original":
+        cmds.append(CMD.format(HL_FILESYSTEMPATH, env, peer_log_path))
+    elif db_type == "ustore":
+        cmds.append(CMD.format(USTORE_DATAPATH, env, peer_log_path))
+    else:
+        print "Unrecognized DB", db_type
+        sys.exit(0)
+
+
+  run_servers(nodes, cmds)
+  time.sleep(10)
+  print "Network Setup"
+
+  # CLIENT_NODE = HOST.format(CLIENT_HOSTNO)
+  # run_clients(CLIENT_NODE, NODES[0], WORKLOAD_FILE, thread, txrate, db_type)
+
+  # time.sleep(SLEEP_TIME)
+
+  # stop(CLIENT_NODE, NODES)
+  # time.sleep(5)
+  # return "{}/client_{}_{}_{}".format(CLIENT_LOG_DIR, NODES[0], thread, txrate)
+
+def get_db_size(path):
+  size = 0
+  for dirpath, dirname, filenames in os.walk(path):
+    for f in filenames:
+      fp = os.path.join(dirpath, f)
+      size += os.path.getsize(fp)
+  return size
+
+
+if __name__ == "__main__":
+  if len(sys.argv) != 7 :
+    print usage
+    sys.exit(1)
+
+  db_type = sys.argv[1]
+  num_node = int(sys.argv[2])
+  num_txn = int(sys.argv[3])
+  num_thread = int(sys.argv[4])
+  txrate = int(sys.argv[5])
+  batch_size = int(sys.argv[6])
+  run_exp(num_node, num_txn, num_thread, txrate, batch_size, db_type)
+
+  raw_input("End the servers and Query the size for DB??")
+  nodes=[]
+  nodes.append(HOST.format(BOOTSTRAP_HOSTNO))
+  for i in range(1,num_node):
+    nodes.append(HOST.format(BOOTSTRAP_HOSTNO+i))
+
+  stop([], nodes)
+  # This python script must run at a hl node.
+  db_size = 0
+  if db_type == "rocksdb" or db_type == "original":
+    db_size = get_db_size(HL_FILESYSTEMPATH)
+  elif db_type == "ustore":
+    db_size = get_db_size(USTORE_DATAPATH)
+  else:
+    print "Unrecognized DB", db_type
+    sys.exit(0)
+  print("db size: {}".format(db_size))
+
